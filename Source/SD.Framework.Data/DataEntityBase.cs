1568c4dbb0239797c9f195b207cc6d16c070b751
Sergey Dzyuban <sergey.dzyuban@gmail.com>
2015-02-21 20:07:50 +0200

    Initial commit for common framework project

---
+++ Source/SD.Framework.Data/DataEntityBase.cs
@@ -0,0 +1,25 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DataEntityBase.cs" company="SD">
+//     Copyright (c) 2015. All rights reserved.
+// </copyright>
+// <author>Sergey Dzyuban</author>
+//-----------------------------------------------------------------------
+namespace SD.Framework.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Base class for entity object
+    /// </summary>
+    public class DataEntityBase
+    {
+        /// <summary>
+        /// Gets or sets identity field. 
+        /// </summary>
+        public int Id { get; set; }
+    }
+}
---
+++ Source/Test/SD.Framework.Data.Test/DataEntityBaseTest.cs
@@ -0,0 +1,44 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DataEntityBaseTest.cs" company="SD">
+//     Copyright (c) 2015. All rights reserved.
+// </copyright>
+// <author>Sergey Dzyuban</author>
+//-----------------------------------------------------------------------
+namespace SD.Framework.Data.Test
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class DataEntityBaseTest
+    {
+        [Test]
+        public void Identity_WriteAndRead_SameResult()
+        {
+            DataEntityBase entity = new DataEntityBase();
+            entity.Id = 5;
+
+            Assert.AreEqual(5, entity.Id);
+        }
+
+        [Test]
+        public void Class_CreateWithConstructor_IdentityIsZero()
+        {
+            DataEntityBase entity = new DataEntityBase();
+
+            Assert.AreEqual(0, entity.Id);
+        }
+
+        [Test]
+        public void Class_CreateWithConstructor_CreatedSuccesful()
+        {
+            DataEntityBase entity = new DataEntityBase();
+
+            Assert.IsNotNull(entity);
+        }
+    }
+}
